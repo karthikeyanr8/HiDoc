1032e8bb8b2a8dddac98ce2f5b17bcbf2e3bcebe
karthikeyanr8 <146131947+karthikeyanr8@users.noreply.github.com>
2026-04-21 22:58:01 +0530

    Add files via upload

---
+++ ConsulationTool/ConsulationTool/src/main/java/Hidoc/ConsulationTool/ConsulationToolApplication.java
@@ -0,0 +1,13 @@
+package Hidoc.ConsulationTool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsulationToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsulationToolApplication.class, args);
+	}
+
+}
---
+++ ConsulationTool/ConsulationTool/src/test/java/Hidoc/ConsulationTool/ConsulationToolApplicationTests.java
@@ -0,0 +1,13 @@
+package Hidoc.ConsulationTool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsulationToolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
