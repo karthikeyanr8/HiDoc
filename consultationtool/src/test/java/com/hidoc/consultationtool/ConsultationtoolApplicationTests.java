7d0995eab4c0c615171be29a34637b8e34376ef9
karthikeyanr8 <146131947+karthikeyanr8@users.noreply.github.com>
2026-04-21 23:45:45 +0530

    Add files via upload

---
+++ consultationtool/src/test/java/com/hidoc/consultationtool/ConsultationtoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hidoc.consultationtool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsultationtoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
